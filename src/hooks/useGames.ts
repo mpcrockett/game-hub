2aa68687a6e3a1221f5ed597db161c31118fc180
mpcrockett <mpcrockett+github@gmail.com>
2023-03-31 14:41:34 -0400

    display platform icons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,38 +2,41 @@ import { CanceledError } from "axios";
 import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
 export interface Game {
   id: number;
   name: string;
   background_image: string
+  parent_platforms: { platform: Platform }[]
 }
 
 interface GamesResponse {
   count: number;
-  results: Game[]
+  results: Game[];
 }
 
 const useGames = () => {
-const controller = new AbortController();
-
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState('');
 
   useEffect(() => {
+    const controller = new AbortController();
+
     apiClient.get<GamesResponse>('/games', { signal: controller.signal })
       .then(res => setGames(res.data.results))
-      .catch(err => {
-        if(err instanceof CanceledError) {
-          console.log("This is where its happening");
-          return;
-        }; 
+      .catch((err) => {
+        if(err instanceof CanceledError) return;
         setError(err.message);
       });
 
     return () => controller.abort();
   }, []);
 
-  return { games, error }
+  return { games, error };
 }
 
 export default useGames;
