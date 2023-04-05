881cf860fb4dfb4bd75001858a0152947fb25bf7
mpcrockett <mpcrockett+github@gmail.com>
2023-04-05 14:27:36 -0400

    clean up game cards

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,26 +1,30 @@
-import { Card, CardBody, Heading, HStack, Image, Text } from '@chakra-ui/react'
-import { Game } from '../hooks/useGames'
-import getCroppedImageUrl from '../services/image-url';
-import CriticScore from './CriticScore';
-import PlatformIconList from './PlatformIconList';
+import { Card, CardBody, Heading, HStack, Image, Text } from "@chakra-ui/react";
+import { Game } from "../hooks/useGames";
+import getCroppedImageUrl from "../services/image-url";
+import CriticScore from "./CriticScore";
+import PlatformIconList from "./PlatformIconList";
 
 interface Props {
-  game: Game,
+  game: Game;
 }
 
 function GameCard({ game }: Props) {
+  const headingSize = game.name.length > 27 ? 'l' : 'xl';
+
   return (
-    <Card> 
+    <Card>
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
-        <Heading fontSize='xl'>{game.name}</Heading>
-        <HStack justifyContent='space-between'>
-          <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)}/>
+        <HStack justifyContent="space-between" marginBottom={3}>
+          <PlatformIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
           <CriticScore score={game.metacritic} />
         </HStack>
+        <Heading fontSize={headingSize}>{game.name}</Heading>
       </CardBody>
     </Card>
   );
 }
 
-export default GameCard
+export default GameCard;
